837778a43327c75c943320df925ec6b0ed8d83cc
Brenna Pavlinchak <littlemeg27@student.fullsail.edu>
2024-08-23 01:06:01 -0400

    Working on adding MapBox

--- CrimeShield/settings.gradle.kts
+++ CrimeShield/settings.gradle.kts
@@ -2,6 +2,9 @@
     "RedundantSuppression", "RedundantSuppression", "RedundantSuppression"
 )
 
+import org.gradle.internal.impldep.org.jsoup.safety.Safelist.basic
+
+
 pluginManagement {
     repositories {
         google()
@@ -15,6 +18,20 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+
+        maven {
+            url 'https://api.mapbox.com/downloads/v2/releases/maven'
+            authentication {
+                basic(BasicAuthentication)
+            }
+            credentials {
+                // Do not change the username below.
+                // This should always be `mapbox` (not your username).
+                username = "mapbox"
+                // Use the secret token you stored in gradle.properties as the password
+                password = MAPBOX_DOWNLOADS_TOKEN
+            }
+        }
     }
 }
 
