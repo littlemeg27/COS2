ecf64b79682f5277c067112346207166373955fd
Brenna Pavlinchak <littlemeg27@student.fullsail.edu>
2024-08-29 14:41:55 -0400

    Went back to past branch, uninstalled Android Studio,

--- CrimeShield/app/build.gradle.kts
+++ CrimeShield/app/build.gradle.kts
@@ -84,7 +84,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation ("androidx.compose.material:material-icons-extended:1.6.8")
-    implementation("com.mapbox.maps:android:10.14.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
